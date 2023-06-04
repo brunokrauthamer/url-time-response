9916b07f8af42b510b927e5db2be18cc308807ef
Bruno Krauthamer <brunokrauthamer@gmail.com>
2023-06-04 19:32:37 -0300

    methdo generateToken created into TokenService file2

---
+++ back-end/Services/TokenService.cs
@@ -0,0 +1,29 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using TimeResponse.Models;
+
+namespace ResponseTime.Services
+{
+    public static class TokenService
+    {
+        public static string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("your_secret_key_here"); // Chave secreta para a assinatura do token
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, user.Username),
+                }),
+                Expires = DateTime.UtcNow.AddDays(1), // Define a validade do token
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}

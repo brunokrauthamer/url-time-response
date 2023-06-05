a3765e550ae6e44b30e31dda06b5723df07cfcf0
Bruno Krauthamer <brunokrauthamer@gmail.com>
2023-06-04 21:48:16 -0300

    adding space before 'ms'

--- back-end/Serveices/ResponseTimeService.cs
+++ back-end/Serveices/ResponseTimeService.cs
@@ -38,7 +38,7 @@ namespace ResponseTime.Services
 
                 var responseTimeInfo = new ResponseTimeModel
                 {
-                    ResponseTime = responseTime.TotalMilliseconds + "ms",
+                    ResponseTime = responseTime.TotalMilliseconds + " ms",
                     Url = url,
                     Responded = false
                 };
